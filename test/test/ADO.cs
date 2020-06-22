53a6832845768e8dd33fed8463fcc0e552867c65
VladislavChepusov <vladislavchepusov@gmail.com>
2020-06-22 11:37:40 +0300

    Интерфейс для бд

---
+++ test/test/ADO.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace test
+{
+    class ADO
+    {
+
+        private string conect;
+        public ADO()
+        {
+            MySqlConnectionStringBuilder mysqlCSB;
+            mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "127.0.0.1";
+            mysqlCSB.Database = "lab17";
+            mysqlCSB.UserID = "root";
+            mysqlCSB.Password = "Work15notwolf";
+            conect = mysqlCSB.ConnectionString;
+         
+        }
+        private string first_request = @"SELECT * FROM обои";
+        private string second_request = @"SELECT * FROM партия";
+
+        //datagrid
+        public DataTable ForDataGrid()
+        {
+            DataTable dt = new DataTable();
+
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = conect;
+               // Открываем соединение с базой данных
+                con.Open();
+
+                MySqlCommand com = new MySqlCommand(first_request, con);
+
+                using (MySqlDataReader dr = com.ExecuteReader())
+                {
+                    ////есть записи?
+                    if (dr.HasRows)
+                    {
+                        ////заполняем объект DataTable
+                        dt.Load(dr);
+                    }
+                    return dt;
+                }
+            }
+        }
+
+
+
+        public List<String> mySqlCommand()
+        {
+            
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = conect;
+                con.Open();
+
+                List<String> result = new List<string>();
+
+                MySqlCommand com = new MySqlCommand(second_request, con);
+
+                /*
+                MySqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Add(reader[0].ToString());
+                }
+            */
+
+
+                using (MySqlDataReader reader = com.ExecuteReader())
+
+                {
+                    result.Add("|");
+                    result.Add("id");
+                    result.Add("id");
+                    result.Add("Name");
+                    result.Add("Data");
+                    result.Add("|");
+
+                    while (reader.Read())
+                    {
+
+                        result.Add("|");
+                        result.Add(reader[0].ToString());
+                        result.Add(reader[1].ToString());
+                        result.Add(reader[2].ToString());
+                        result.Add(reader[3].ToString());
+                        result.Add("|");
+
+                    }
+                }
+
+                return result;
+            }
+        }
+
+
+
+    }
+}
---
+++ test/test/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            using (lab17Entities1 db= new lab17Entities1())
+            {
+                var производитель = db.производитель;
+
+                foreach (производитель s in производитель)
+                {
+                    listView1.Items.Add(s.ID_Prod.ToString()); //+ "\n " + s.Name_Prod.ToString() + ' ' + s.Type.ToString() + ' '+s.Data_creature.ToString());
+                    listView1.Items.Add(s.Name_Prod.ToString());
+                    listView1.Items.Add(s.Data_creature.ToString());
+                    listView1.Items.Add(s.Type.ToString());
+
+                }
+            }
+        }
+        private ADO db = new ADO();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //   dataGrid view        
+            dataGridView1.DataSource = db.ForDataGrid();
+
+            //   SqlCommand     
+            listView2.Items.Clear();
+            listView2.BeginUpdate();
+            foreach (var wall in db.mySqlCommand())
+            {
+                var item = new ListViewItem();
+                item.Text = wall;
+                listView2.Items.Add(item);
+            }
+            listView2.EndUpdate();
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
